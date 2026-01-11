5a3da2511d7b8e4e4a56a621e86564e6373bac30
RupiV <rupendra.v1069@gmail.com>
2026-01-11 09:38:25 +0530

    Rupendra

---
+++ src/app/shared/components/reports/ward-wise-licenses/ward-wise-licenses.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WardWiseLicensesService {
+
+  private baseUrl = 'https://localhost:7181/api'; // 👈 change to your backend
+
+  constructor(private http: HttpClient) {}
+
+  get<T>(url: string) {
+    return this.http.get<T>(`${this.baseUrl}${url}`);
+  }
+
+  post<T>(url: string, body: any) {
+    return this.http.post<T>(`${this.baseUrl}${url}`, body);
+  }
+
+  put<T>(url: string, body: any) {
+    return this.http.put<T>(`${this.baseUrl}${url}`, body);
+  }
+}
